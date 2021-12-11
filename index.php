f727b3bee7c0fbb20b02e871fc00d42affe886a8
Patricio Aguilar <94617663+PatricioAguilarVargas@users.noreply.github.com>
2021-12-11 13:51:08 -0300

    commit

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <?php //var_dump( $_SERVER) ?>
 <html>
 	<head>
+		<link rel="icon" href="favicon.ico">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	</head>
 	<body>
